0b3b79e8f0e9e83edece5693cf4b41170f1c8319
joaojorte <rafaeljorte@gmail.com>
2026-09-11 03:57:20 -0400

    auto-sync: atualizar src/utils/constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,34 @@
+export const CATEGORIAS_ENTRADA = [
+  'Salário',
+  'Dividendos',
+  'Rendimentos',
+  'Estorno/Devolução',
+  'Outros'
+];
+export const CATEGORIA_ENTRADA_PADRAO = 'Outros';
+
+export const CATEGORIAS_SAIDA = [
+  'Alimentação',
+  'Mercado',
+  'Transporte',
+  'Saúde',
+  'Educação',
+  'Comunicação',
+  'Compras',
+  'Serviços',
+  'Transferências/Pagamentos pessoais',
+  'Outros',
+  'Não identificado'
+];
+export const CATEGORIA_SAIDA_PADRAO = 'Não identificado';
+
+export const PERCENTUAL_INVESTIMENTO_PADRAO = 20;
+export const PERCENTUAL_RESERVA_PADRAO = 10;
+export const TAXA_MENSAL_PADRAO = 0.8;
+export const PRAZO_ANOS_PADRAO = 10;
+
+export const STORAGE_KEYS = {
+  LOCAL_DATA: 'moneyhub_dados_locais',
+  CARTAO_DIA_VENCIMENTO: 'moneyhub_cartao_dia_vencimento',
+  INVESTIMENTOS: 'moneyhub_investimentos_historico'
+};
